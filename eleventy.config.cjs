0b8a57f663c3b045177085af14da3cdf80e57f87
axemonkey <hi+github@clivemurray.com>
2025-08-26 15:09:44 +0100

    first commit

---
+++ eleventy.config.cjs
@@ -0,0 +1,19 @@
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy('src/public');
+	eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
+	eleventyConfig.setUseGitIgnore(false);
+	eleventyConfig.setServerOptions({
+		// liveReload: false,
+		watch: [
+			'src/public/**/*',
+		],
+		showVersion: true,
+	});
+
+	return {
+		dir: {
+			includes: "_includes",
+			layouts: "_layouts",
+		}
+	}
+};
